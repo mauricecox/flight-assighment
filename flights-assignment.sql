f693c4b4d29fa42da2a5ae33af7d4d8bc8afdda6
“Maurice <maurice.cox@verizon.net>
2015-09-06 18:48:26 -0600

    Flights Homework
    Version 1

---
+++ flights-assignment.sql
@@ -0,0 +1,50 @@
+-- Maurice Cox
+-- Flights Assignment
+
+
+-- problem 1
+-- How many airplanes have listed speeds?
+-- What is the minimum listed speed and the maximum listed speed?
+SELECT * FROM planes WHERE speed IS NOT NULL
+ORDER BY speed ASC;
+
+
+-- problem 2
+-- What is the total distance flown by all of the planes in January 2013? 
+SELECT SUM(distance) FROM flights WHERE year=2013 AND month=1;
+
+-- What is the total distance flown by all of the planes in January 2013 where the tailnum is missing?
+SELECT SUM(distance) FROM flights WHERE year=2013 AND month=1 AND tailnum IS NULL;
+
+-- problem 3
+-- What is the total distance flown by all of the planes in July 5, 2013? 
+SELECT * FROM flights WHERE year = 2013 AND month=7 AND day=5;
+SELECT * FROM planes;
+
+-- Inner Join
+SELECT manufacturer, distance
+FROM planes 
+INNER JOIN flights
+ON planes.tailnum=flights.tailnum
+WHERE flights.year=2013 AND flights.month=7 AND flights.day=5
+GROUP BY manufacturer ASC;
+
+
+-- Left Outer Join
+SELECT manufacturer, distance
+FROM planes 
+LEFT OUTER JOIN flights
+ON planes.tailnum=flights.tailnum
+WHERE flights.year=2013 AND flights.month=7 AND flights.day=5
+GROUP BY manufacturer ASC;
+
+-- problem 4
+-- trying to select airplanes that fly into EWR airport and see what the weather is on July 5th
+
+SELECT *
+FROM planes
+JOIN flights
+ON planes.tailnum=flights.tailnum
+JOIN weather
+ON weather.orgin=planes.orgin
+WHERE flights.year=2013 AND flights.month=7 AND flights.day=5 AND orgin=EWR;
